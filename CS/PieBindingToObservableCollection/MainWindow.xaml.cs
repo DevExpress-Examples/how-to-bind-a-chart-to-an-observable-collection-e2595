4e7c5cf5dd3b42e183a4f980534b4f8eb206e95c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PieBindingToObservableCollection/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Collections.ObjectModel;
+// ...
+
+namespace PieBindingToObservableCollection {
+    public partial class MainWindow : Window {
+        public static readonly DependencyProperty PointsCollectionProperty = DependencyProperty.Register("PointsCollection",
+            typeof(PointsCollection), typeof(MainWindow), new PropertyMetadata());
+
+        public PointsCollection PointsCollection {
+            get { return (PointsCollection)GetValue(PointsCollectionProperty); }
+            set { SetValue(PointsCollectionProperty, value); }
+        }
+
+        public MainWindow() {
+            InitializeComponent();
+            PointsCollection = new PointsCollection();
+            PointsCollection.Add(new Point(1, 1));
+            PointsCollection.Add(new Point(2, 2));
+            PointsCollection.Add(new Point(3, 3));
+        }
+        void Button_Click(object sender, RoutedEventArgs e) {
+            PointsCollection.Add(new Point(1, 1));
+        }
+    }
+
+    public class PointsCollection : ObservableCollection<Point> {
+    }
+}

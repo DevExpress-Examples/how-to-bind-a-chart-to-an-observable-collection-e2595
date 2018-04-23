220e8942db5be6dd2c87e70c34585d334e096554
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PieBindingToObservableCollection/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Collections.ObjectModel;
+// ...
+
+namespace PieBindingToObservableCollection {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+
+    public class ViewModel {
+        public ObservableCollection<Point> PointsCollection { get; private set; }
+
+        public ViewModel() {
+            PointsCollection = new ObservableCollection<Point> { new Point(1, 1), new Point(2, 2), new Point(3, 3) };
+        }
+
+        public void AddPoint() {
+            PointsCollection.Add(new Point(1, 1));
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to bind a chart to an observable collection
+
+
+<p>This example illustrates how to create a Pie chart bound to an observable collection.</p>
+<p>In this example, click the button, to add points to the existing collection, and view the resulting chart.<br><br><strong>[Update]</strong> (v17.1) The <a href="https://documentation.devexpress.com/#WPF/CustomDocument17352">POCO ViewModel</a> approach is used to define the Series data source and attach the AddPoint command to the Button.</p>
+
+<br/>
+
+

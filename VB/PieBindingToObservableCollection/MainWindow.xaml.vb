256cbc16363192b712740f4d7851fce5fd5c6866
DevExpressExampleBot <fakeEmail>
2021-09-01 18:12:32 +0300

    Source auto update [skip ci]

--- VB/PieBindingToObservableCollection/MainWindow.xaml.vb
+++ VB/PieBindingToObservableCollection/MainWindow.xaml.vb
@@ -3,35 +3,35 @@ Imports System.Collections.ObjectModel
 ' ...
 
 Namespace PieBindingToObservableCollection
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
 
-    Public Class ViewModel
-        Private privatePointsCollection As ObservableCollection(Of Point)
-        Public Property PointsCollection() As ObservableCollection(Of Point)
-            Get
-                Return privatePointsCollection
-            End Get
-            Private Set(ByVal value As ObservableCollection(Of Point))
-                privatePointsCollection = value
-            End Set
-        End Property
+	Public Class ViewModel
+		Private privatePointsCollection As ObservableCollection(Of Point)
+		Public Property PointsCollection() As ObservableCollection(Of Point)
+			Get
+				Return privatePointsCollection
+			End Get
+			Private Set(ByVal value As ObservableCollection(Of Point))
+				privatePointsCollection = value
+			End Set
+		End Property
 
-        Public Sub New()
-            PointsCollection = New ObservableCollection(Of Point) From { _
-                New Point(1, 1), _
-                New Point(2, 2), _
-                New Point(3, 3) _
-            }
-        End Sub
+		Public Sub New()
+			PointsCollection = New ObservableCollection(Of Point) From {
+				New Point(1, 1),
+				New Point(2, 2),
+				New Point(3, 3)
+			}
+		End Sub
 
-        Public Sub AddPoint()
-            PointsCollection.Add(New Point(1, 1))
-        End Sub
-    End Class
+		Public Sub AddPoint()
+			PointsCollection.Add(New Point(1, 1))
+		End Sub
+	End Class
 End Namespace

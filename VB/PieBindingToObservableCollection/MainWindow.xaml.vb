c9fc46f814ea8688fb0618d0bf37e3fa787da2b1
DevExpressExampleBot <fakeEmail>
2025-03-07 14:28:38 +0400

    Source auto update [skip ci]

--- VB/PieBindingToObservableCollection/MainWindow.xaml.vb
+++ VB/PieBindingToObservableCollection/MainWindow.xaml.vb
@@ -1,37 +1,37 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports System.Collections.ObjectModel
+
 ' ...
-
 Namespace PieBindingToObservableCollection
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
 
-	Public Class ViewModel
-		Private privatePointsCollection As ObservableCollection(Of Point)
-		Public Property PointsCollection() As ObservableCollection(Of Point)
-			Get
-				Return privatePointsCollection
-			End Get
-			Private Set(ByVal value As ObservableCollection(Of Point))
-				privatePointsCollection = value
-			End Set
-		End Property
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 
-		Public Sub New()
-			PointsCollection = New ObservableCollection(Of Point) From {
-				New Point(1, 1),
-				New Point(2, 2),
-				New Point(3, 3)
-			}
-		End Sub
+    Public Class ViewModel
 
-		Public Sub AddPoint()
-			PointsCollection.Add(New Point(1, 1))
-		End Sub
-	End Class
+        Private _PointsCollection As ObservableCollection(Of System.Windows.Point)
+
+        Public Property PointsCollection As ObservableCollection(Of Point)
+            Get
+                Return _PointsCollection
+            End Get
+
+            Private Set(ByVal value As ObservableCollection(Of Point))
+                _PointsCollection = value
+            End Set
+        End Property
+
+        Public Sub New()
+            PointsCollection = New ObservableCollection(Of Point) From {New Point(1, 1), New Point(2, 2), New Point(3, 3)}
+        End Sub
+
+        Public Sub AddPoint()
+            PointsCollection.Add(New Point(1, 1))
+        End Sub
+    End Class
 End Namespace
